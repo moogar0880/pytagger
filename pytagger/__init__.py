9bcfced5c9deeb9219786be70226ad7426ff77ca
moogar0880 <moogar@comcast.net>
2014-06-22 18:44:03 -0400

    Version bump for config parsing addition

--- pytagger/__init__.py
+++ pytagger/__init__.py
@@ -5,7 +5,7 @@ except ImportError:
     # Don't let dependencies stop us from importing on install
     pass
 
-version_info = (1, 0, 3)
+version_info = (1, 0, 4)
 
 __name__ = 'pytagger'
 __doc__ = 'A python backend to iTunes style metadata tagging'
