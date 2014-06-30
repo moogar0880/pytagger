fffa0f99d571dbfa8319786d0ff55fee6d34aadb
moogar0880 <moogar@comcast.net>
2014-06-30 18:37:25 -0400

    Added a few performance improvements to setup.py and added some additional metadata.

--- setup.py
+++ setup.py
@@ -10,11 +10,13 @@ requires = ['fuzzywuzzy', 'requests', 'trakt', 'subler']
 
 itunes = 'git+https://github.com/moogar0880/python-itunes#egg=python-itunes-1.1'
 
-install_cmd = 'pip install %s'
-if os.getuid() == 0:
-    install_cmd = 'sudo pip install %s'
-
-subprocess.check_call(install_cmd % itunes, shell=True)
+try:
+    import itunes
+except ImportError:
+    install_cmd = 'pip install %s'
+    if os.getuid() == 0:
+        install_cmd = 'sudo pip install %s'
+    subprocess.check_call(install_cmd % itunes, shell=True)
 
 setup(name='pytagger',
       version=__version__,
@@ -24,10 +26,12 @@ setup(name='pytagger',
       maintainer='Jonathan Nappi',
       maintainer_email='moogar@comcast.net',
       license='http://www.gnu.org/copyleft/gpl.html',
+      package_dir={'pytagger': 'pytagger'},
       platforms=['OS X'],
       url='https://github.com/moogar0880/pytagger',
       packages=packages,
       install_requires=requires,
+      zip_safe=False,
       entry_points={
           'console_scripts': ['tag = pytagger.main:main']
       },
